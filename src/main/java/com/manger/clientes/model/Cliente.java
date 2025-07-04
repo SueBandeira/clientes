fe11c8dfcbe530f277b180aa7eb8c62522870266
SueBandeira <sbandeira099@gmail.com>
2025-07-04 18:47:51 -0300

    Inserção preliminar de dados

---
+++ src/main/java/com/manger/clientes/model/Cliente.java
@@ -0,0 +1,54 @@
+package com.manger.clientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+ // private DadosEndereco endereco;
+  private String telefone;
+  private String email;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+//  public DadosEndereco getEndereco() {
+//    return endereco;
+//  }
+//
+//  public void setEndereco(DadosEndereco endereco) {
+//    this.endereco = endereco;
+//  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
---
+++ src/main/java/com/manger/clientes/model/Usuario.java
@@ -0,0 +1,37 @@
+package com.manger.clientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+  private String senha;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+}
