03337e6550e6da1fc5ec51e4f480631c306388e0
SueBandeira <sbandeira099@gmail.com>
2025-07-04 17:57:46 -0300

    Initial commit

---
+++ src/test/java/com/manger/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manger.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
